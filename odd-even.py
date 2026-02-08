b2ff71d6e34c165c8799831ef529de45b4d63b76
shwetasb1412 <shwetasb1412@gmail.com>
2026-02-08 13:22:43 +0530

    Add files via upload

---
+++ odd-even.py
@@ -0,0 +1,5 @@
+for i in range(1,21):
+    if i%2==0:
+        print(str(i)+" is an even number")
+    else:
+        print(str(i)+" is an odd number")
---
+++ table5.py
@@ -0,0 +1,4 @@
+
+for i in range(1,11):
+    n=5*i
+    print("5x"+str(i)+"="+str(n))
